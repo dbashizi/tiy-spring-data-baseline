5c8fc851cadd376d1403863407330c5a5c59454c
Dominique Bashizi <dominique@blupods.com>
2017-02-09 15:03:31 -0500

    Gave the User entity explicit mapping to work around naming conflict

--- src/main/java/com/tiy/entities/User.java
+++ src/main/java/com/tiy/entities/User.java
@@ -3,15 +3,49 @@ package com.tiy.entities;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 /**
  * Created by dbash on 2/9/2017.
  */
 @Entity
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue
     private Long id;
     private String emailAddress;
     private String fullName;
+
+    public User() {
+    }
+
+    public User(String emailAddress, String fullName) {
+        this.emailAddress = emailAddress;
+        this.fullName = fullName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
 }
